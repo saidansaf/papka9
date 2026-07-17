46341542a51bf5ab3036aac0963618f7ab490a93
saidansaf <afzalxonovsaidansaf@gmail.com>
2026-07-17 10:35:01 +0500

    Update urls.py

--- api9/urls.py
+++ api9/urls.py
@@ -2,5 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.ProductViewSet.as_view({"post": "create"})),
+    path("list/", views.ProductListAPIView.as_view()),
+    path("create/", views.ProductCreateAPIView.as_view()),
+    path("<int:pk>/retrieve/", views.ProductRetrieveAPIView.as_view()),
+    path("<int:pk>/update/", views.ProductUpdateAPIView.as_view()),
+    path("<int:pk>/delete/", views.ProductDestroyAPIView.as_view()),
+    path("list-create/", views.ProductListCreateAPIView.as_view()),
+    path("<int:pk>/retrieve-update/", views.ProductRetrieveUpdateAPIView.as_view()),
+    path("<int:pk>/retrieve-delete/", views.ProductRetrieveDestroyAPIView.as_view()),
+    path("<int:pk>/retrieve-update-delete/", views.ProductRetrieveUpdateDestroyAPIView.as_view()),
 ]
